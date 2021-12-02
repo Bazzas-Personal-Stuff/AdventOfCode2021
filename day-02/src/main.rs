623fd96ca06b0345246b754a6b1bb1bde7f732b5
Mungo_ <bailey.gibbons2018@gmail.com>
2021-12-02 17:11:52 +1100

    Finished day 2

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{BufRead, BufReader, Lines};
+use std::io::{BufRead, BufReader};
 
 struct Transform {
     x: i64,
@@ -7,57 +7,39 @@ struct Transform {
     aim: i64
 }
 
-fn stage_1(){
-    let mut transform = Transform { x: 0, y: 0, aim: 0 };
-
-    let reader = BufReader::new(
-        File::open("input.txt")
-            .expect("Error opening file"));
-
-    for line in reader.lines() {
-        let line_str = line.unwrap();
-        let split: Vec<&str> = line_str.split_whitespace().collect();
-        let dist = split[1].parse::<i64>().unwrap();
-        match split[0] {
-            "forward" =>  transform.x += dist,
-            "up" => transform.y -= dist,
-            "down" => transform.y += dist,
-            _ => println!("Unexpected instruction")
-        }
-    }
-    println!("Stage 1 result: {}", transform.x * transform.y);
-}
-
-
-fn stage_2(){
-    let mut transform = Transform { x: 0, y: 0, aim: 0 };
-
-    let reader = BufReader::new(
-        File::open("input.txt")
-            .expect("Error opening file"));
-
-    for line in reader.lines() {
-        let line_str = line.unwrap();
-        let split: Vec<&str> = line_str.split_whitespace().collect();
-        let dist = split[1].parse::<i64>().unwrap();
-        match split[0] {
-            "forward" =>  {
-                transform.x += dist;
-                transform.y += transform.aim * dist;
-            },
-            "up" => transform.aim -= dist,
-            "down" => transform.aim += dist,
-            _ => println!("Unexpected instruction")
-        }
-    }
-
-    println!("Stage 2 result: {}", transform.x * transform.y);
-}
-
 fn main() {
+    let mut stage_1 = Transform { x: 0, y: 0, aim: 0 };
+    let mut stage_2 = Transform { x: 0, y: 0, aim: 0 };
+
+    let reader = BufReader::new(
+        File::open("input.txt")
+            .expect("Error opening file"));
+    let lines = reader.lines();
+    for line in lines{
+        let line_str = line.unwrap();
+        let instruction: Vec<&str> = line_str.split_whitespace().collect();
+        
+        let dist = instruction[1].parse::<i64>().unwrap();
+        match instruction[0] {
+            "forward" =>  {
+                stage_1.x += dist;
+                stage_2.x += dist;
+                stage_2.y += stage_2.aim * dist;
+            },
+            "up" => {
+                stage_1.y -= dist;
+                stage_2.aim -= dist;
+            },
+            "down" => {
+                stage_1.y += dist;
+                stage_2.aim += dist;
+            },
+            _ => println!("Unexpected instruction")
+        }
+    }
 
 
-    stage_1();
-    stage_2();
+    println!("Stage 1 result: {}", stage_1.x * stage_1.y);
+    println!("Stage 2 result: {}", stage_2.x * stage_2.y);
 }
 
