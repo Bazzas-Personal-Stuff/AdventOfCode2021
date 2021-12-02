ee7882a0a1118dae97ab9e2e92356978825ac91f
Mungo_ <bailey.gibbons2018@gmail.com>
2021-12-02 16:41:39 +1100

    Day 02 - Stage 2

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -1,6 +1,5 @@
-use std::env;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Lines};
 
 struct Transform {
     x: i64,
@@ -8,8 +7,7 @@ struct Transform {
     aim: i64
 }
 
-fn main() {
-
+fn stage_1(){
     let mut transform = Transform { x: 0, y: 0, aim: 0 };
 
     let reader = BufReader::new(
@@ -22,11 +20,44 @@ fn main() {
         let dist = split[1].parse::<i64>().unwrap();
         match split[0] {
             "forward" =>  transform.x += dist,
-            "backward" => transform.x -= dist,
             "up" => transform.y -= dist,
             "down" => transform.y += dist,
             _ => println!("Unexpected instruction")
         }
     }
-    println!("Multiplication result: {}", transform.x * transform.y);
+    println!("Stage 1 result: {}", transform.x * transform.y);
 }
+
+
+fn stage_2(){
+    let mut transform = Transform { x: 0, y: 0, aim: 0 };
+
+    let reader = BufReader::new(
+        File::open("input.txt")
+            .expect("Error opening file"));
+
+    for line in reader.lines() {
+        let line_str = line.unwrap();
+        let split: Vec<&str> = line_str.split_whitespace().collect();
+        let dist = split[1].parse::<i64>().unwrap();
+        match split[0] {
+            "forward" =>  {
+                transform.x += dist;
+                transform.y += transform.aim * dist;
+            },
+            "up" => transform.aim -= dist,
+            "down" => transform.aim += dist,
+            _ => println!("Unexpected instruction")
+        }
+    }
+
+    println!("Stage 2 result: {}", transform.x * transform.y);
+}
+
+fn main() {
+
+
+    stage_1();
+    stage_2();
+}
+
