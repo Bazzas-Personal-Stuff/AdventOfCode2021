b572aff51845e0feff25cadadfc94b9ca5e39bb6
Mungo_ <bailey.gibbons2018@gmail.com>
2021-12-02 17:18:23 +1100

    removed builds from vc

--- day-02/target/debug/day-02.d
+++
@@ -1,1 +0,0 @@
-C:\Dev\Competitive\ Programming\AdventOfCode2021\day-02\target\debug\day-02.exe: C:\Dev\Competitive\ Programming\AdventOfCode2021\day-02\src\main.rs
--- day-02/target/debug/deps/day_02.d
+++
@@ -1,5 +0,0 @@
-C:\Dev\Competitive Programming\AdventOfCode2021\day-02\target\debug\deps\day_02.exe: src\main.rs
-
-C:\Dev\Competitive Programming\AdventOfCode2021\day-02\target\debug\deps\day_02.d: src\main.rs
-
-src\main.rs:
